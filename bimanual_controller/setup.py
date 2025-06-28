667f5897a1579a6a00bfbdf583323a8f5dd1b66a
Aditya Nair <aditya.nair0123@gmail.com>
2025-06-28 17:45:27 -0400

    Created skeleton

--- bimanual_controller/setup.py
+++ bimanual_controller/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'limb_mockup = bimanual_controller.limb_mockup_node:main',
         ],
     },
 )
