a5bf98d92566c4619e4f4e70438bfe514fb85c6b
Aditya Nair <aditya.nair0123@gmail.com>
2025-06-29 11:30:04 -0400

    integrated hardware

--- bimanual_controller/launch/limbs_mockup.launch.py
+++ bimanual_controller/launch/limbs_mockup.launch.py
@@ -19,72 +19,28 @@ def generate_launch_description():
 
     use_rviz = LaunchConfiguration('use_rviz')
 
-    left_offset = ['-0.3', '0.0', '0.0', '0.0', '0.0', '0.7071', '0.7071'] # [x, y, z, qx, qy, qz, qw]
-    right_offset = ['0.3', '0.0', '0.0', '0.0', '0.0', '0.7071', '0.7071'] # [x, y, z, qx, qy, qz, qw]
-
     # Paths to left and right limb xacros
-    left_xacro = PathJoinSubstitution([
+    system_xacro = PathJoinSubstitution([
         FindPackageShare('bimanual_controller'),
-        'urdf', 'left_limb.xacro'
+        'urdf', 'bimanual_system.xacro'
     ])
 
-    right_xacro = PathJoinSubstitution([
-        FindPackageShare('bimanual_controller'),
-        'urdf', 'right_limb.xacro'
-    ])
-
-    # Generate robot descriptions using xacro
-    left_robot_description = {
+    # Generate robot description using combined xacro
+    system_robot_description = {
         'robot_description': ParameterValue(
-            Command(['xacro ', left_xacro]),
-            value_type=str
-        )
-    }
-    right_robot_description = {
-        'robot_description': ParameterValue(
-            Command(['xacro ', right_xacro]),
+            Command(['xacro ', system_xacro]),
             value_type=str
         )
     }
 
-    # Left robot state publisher
-    left_rsp = Node(
+    # System robot state publisher
+    system_rsp = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         namespace='left',
         name='robot_state_publisher',
-        parameters=[left_robot_description],
+        parameters=[system_robot_description],
         output='screen'
-    )
-
-    # Right robot state publisher
-    right_rsp = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        namespace='right',
-        name='robot_state_publisher',
-        parameters=[right_robot_description],
-        output='screen'
-    )
-
-    # OFFSET BROADCASTERS
-
-    # ---- World to Left Offset Link TF ----
-    left_offset_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='left_offset_tf',
-        # condition=IfCondition(<optional>),
-        arguments=[left_offset[0], left_offset[1], left_offset[2], left_offset[3], left_offset[4], left_offset[5], left_offset[6], 'world', 'left_offset_link']
-    )
-
-    # ---- World to Right Offset Link TF ----
-    right_offset_tf = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='right_offset_tf',
-        # condition=IfCondition(<optional>),
-        arguments=[right_offset[0], right_offset[1], right_offset[2], right_offset[3], right_offset[4], right_offset[5], right_offset[6], 'world', 'right_offset_link']
     )
 
     # RVIZ
@@ -103,10 +59,7 @@ def generate_launch_description():
             default_value='true',
             description='Whether to launch RViz'
         ),
-        left_rsp,
-        right_rsp,
-        left_offset_tf,
-        right_offset_tf,
+        system_rsp,
         rviz,
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
